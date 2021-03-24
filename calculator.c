4022bc4b0d7c34a1f4f746349e9fe9bf4dd57573
LAYANI <61026156+0XLAYA@users.noreply.github.com>
2021-03-24 17:12:47 +0100

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,19 +3,23 @@
 
 //Functions
 
-double add(double n1, double n2){
+double add(double n1, double n2)
+{
     double result = n1 + n2;
     return result;
 }
-double sub(double n1, double n2){
+double sub(double n1, double n2)
+{
     double result = n1 - n2;
     return result;
 }
-double multi(double n1, double n2){
+double multi(double n1, double n2)
+{
     double result = n1 * n2;
     return result;
 }
-double divide(double n1, double n2){
+double divide(double n1, double n2)
+{
     double result = n1 / n2;
     return result;
 }
@@ -26,43 +30,48 @@ int main()
     printf("---------------------------------------------------------\n");
     printf("Type 1 for ADDITION | 2 for SUBTRACTION | 3 for MULTIPLICATION and 4 for DIVISION\n===>");
     scanf("%d",&choice);
-    if(choice == 1){
+    if(choice == 1)
+    {
         double n1;
         double n2;
         printf("ENTER FIRST NUMBER: ");
         scanf("%lf", &n1);
-        printf("ENTER SSECOND NUMBER: ");
+        printf("ENTER SECOND NUMBER: ");
         scanf("%lf", &n2);
         printf("%f + %f = %f",n1 , n2, add(n1,n2));
     }
-    else if(choice == 2){
+    else if(choice == 2)
+    {
         double n1;
         double n2;
         printf("ENTER FIRST NUMBER: ");
         scanf("%lf", &n1);
-        printf("ENTER SSECOND NUMBER: ");
+        printf("ENTER SECOND NUMBER: ");
         scanf("%lf", &n2);
         printf("%f - %f = %f",n1 , n2, sub(n1,n2));
     }
-    else if(choice == 3){
+    else if(choice == 3)
+    {
         double n1;
         double n2;
         printf("ENTER FIRST NUMBER: ");
         scanf("%lf", &n1);
-        printf("ENTER SSECOND NUMBER: ");
+        printf("ENTER SECOND NUMBER: ");
         scanf("%lf", &n2);
         printf("%f X %f = %f",n1 , n2, multi(n1,n2));
     }
-    else if(choice == 4){
+    else if(choice == 4)
+    {
         double n1;
         double n2;
         printf("ENTER FIRST NUMBER: ");
         scanf("%lf", &n1);
-        printf("ENTER SSECOND NUMBER: ");
+        printf("ENTER SECOND NUMBER: ");
         scanf("%lf", &n2);
         printf("%f / %f = %f",n1 , n2, divide(n1,n2));
     }
-    else{
+    else
+    {
         printf("                      INVALID INPUT! ENTER 1,2,3 or 4\n");
         printf("                      -----------------------------------");
         printf("\n");
